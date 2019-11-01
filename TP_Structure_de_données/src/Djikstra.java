27dc20d0b766732c1dd6b5e3ecdd3a54dba57933
LefeverCharles <55986806+LefeverCharles@users.noreply.github.com>
2019-11-01 15:57:01 +0100

    Add files via upload

---
+++ TP_Structure_de_données/src/Djikstra.java
@@ -0,0 +1,94 @@
+import java.util.*;
+
+public class Djikstra {
+    static HashMap<Character, Integer[]> map = new HashMap<Character,Integer[]>();
+    static Character[] index = {'A','B','C','D','E','F','G','H','I','J'};
+    static ArrayDeque<Character> queue = new ArrayDeque<Character>();
+    static ArrayDeque<Character> seen = new ArrayDeque<>();
+    static TreeMap<Character,ArrayDeque<Character>> shorterPath = new TreeMap<Character,ArrayDeque<Character>>();
+    static TreeMap<Character,Integer> distance = new TreeMap<Character,Integer>();
+
+    public static void main(String[] args){
+        gen_map();
+        queue.addFirst('A');
+    }
+
+    static void gen_map(){
+        map.put('A', new Integer[]{-1,85,217,-1,173,-1,-1,-1,-1,-1});
+        map.put('B', new Integer[]{85,-1,-1,-1,-1,80,-1,-1,-1,-1});
+        map.put('C', new Integer[]{217,-1,-1,-1,-1,-1,186,103,-1,-1});
+        map.put('D', new Integer[]{-1,-1,-1,-1,-1,-1,-1,183,-1,-1});
+        map.put('E', new Integer[]{173,-1,-1,-1,-1,-1,-1,-1,-1,502});
+        map.put('F', new Integer[]{-1,80,-1,-1,-1,-1,-1,-1,250,-1});
+        map.put('G', new Integer[]{-1,-1,186,-1,-1,-1,-1,-1,-1,-1});
+        map.put('H', new Integer[]{-1,-1,103,183,-1,-1,-1,-1,-1,167});
+        map.put('I', new Integer[]{-1,-1,-1,-1,250,-1,-1,-1,84});
+        map.put('J', new Integer[]{-1,-1,-1,-1,502,-1,-1,167,84,-1});
+        shorterPath.put('A', new ArrayDeque<Character>(Collections.singletonList('A')));
+        shorterPath.put('B',new ArrayDeque<Character>());
+        shorterPath.put('C',new ArrayDeque<Character>());
+        shorterPath.put('D',new ArrayDeque<Character>());
+        shorterPath.put('E',new ArrayDeque<Character>());
+        shorterPath.put('F',new ArrayDeque<Character>());
+        shorterPath.put('G',new ArrayDeque<Character>());
+        shorterPath.put('H',new ArrayDeque<Character>());
+        shorterPath.put('I',new ArrayDeque<Character>());
+        shorterPath.put('J',new ArrayDeque<Character>());
+        distance.put('A',0);
+        distance.put('B', Integer.MAX_VALUE);
+        distance.put('C', Integer.MAX_VALUE);
+        distance.put('D', Integer.MAX_VALUE);
+        distance.put('E', Integer.MAX_VALUE);
+        distance.put('F', Integer.MAX_VALUE);
+        distance.put('G', Integer.MAX_VALUE);
+        distance.put('H', Integer.MAX_VALUE);
+        distance.put('I', Integer.MAX_VALUE);
+        distance.put('J', Integer.MAX_VALUE);
+    }
+
+    static int getIndex (Character x){
+        for (int i = 0; true; i++){
+            if (index[i]== x) {
+                return i;
+            }
+        }
+    }
+
+    static ArrayDeque<Character> findVoisins (Character node){
+        Integer[] dist = map.get(node);
+        ArrayDeque<Character> voisins = new ArrayDeque<>();
+        for(int i = 0; i< 10;i ++){
+            if (index[i]>0) {
+                voisins.addFirst(index[i]);
+            }
+        }
+        return voisins;
+    }
+
+    static Integer findDistance (Character nodeA, Character nodeB){
+        int index = getIndex(nodeB);
+        return map.get(nodeA)[index];
+    }
+
+    static void
+
+    static void djikstra (ArrayDeque<Character> queue) {
+        Character node = queue.removeFirst();
+        ArrayDeque<Character> voisins = findVoisins(node);
+        seen.addFirst(node);
+        for (Character voisin: voisins){
+            Integer dist = findDistance(node, voisin);
+            if (distance.get(voisin)>distance.get(node)+dist){
+                distance.put(voisin, distance.get(node)+dist);
+                ArrayDeque<Character> newPath = shorterPath.get(node).clone();
+                newPath.addLast(voisin);
+                shorterPath.put(voisin, newPath);
+            }
+            if (!seen.contains(voisin) && !queue.contains(voisin)){
+                queue.addFirst(voisin);
+            }
+        }
+
+    }
+}
+
---
+++ TP_Structure_de_données/src/Main.java
@@ -0,0 +1,151 @@
+import java.util.*;
+
+
+public class Main {
+    static char [] [] plateau = new char[8][8];
+    static int [] position = new int[2];
+    static char direction ='E';
+    static TreeMap<Integer, Integer[]> map = new TreeMap<Integer, Integer[]>();
+    static Scanner scanner = new Scanner(System.in);
+
+    static void initialisation() {
+        for(int i=0; i<8;i++){
+            for(int j=0; j<8;j++){
+                plateau[i][j] = ' ';
+            }
+        }
+        position = new int[]{7, 0};
+    }
+
+    static ArrayDeque<String> creationFile(){
+        ArrayDeque<String> file = new ArrayDeque<String>();
+        while(file.size()<5){
+        String act = scanner.nextLine();
+        if(act.equals("A") || act.equals("G") || act.equals("D")){
+            file.addFirst(act);
+            }
+        }
+        return file;
+    }
+
+    static void deplacement(ArrayDeque<String> instructions){
+        while(instructions.size()!=0){
+            String act = instructions.removeLast();
+            if(act.equals("A")){
+                if(direction == 'N'){
+                    if(position[0] != 0){
+                        position[0]--;
+                    }
+                }
+                if(direction == 'S'){
+                    if(position[0] != 7){
+                        position[0]++;
+                    }
+                }
+                if(direction == 'O'){
+                    if(position[1] !=0){
+                        position[1]--;
+                    }
+                }
+                if(direction == 'E'){
+                    if(position[1] !=7){
+                        position[1]--;
+                    }
+                }
+            }
+            if(act.equals("G")){
+                if(direction == 'N'){
+                    direction = 'O';
+                    }
+                if(direction == 'O'){
+                    direction = 'S';
+                }
+                if(direction == 'S'){
+                    direction = 'E';
+                }
+                if(direction == 'E'){
+                    direction = 'N';
+                }
+            }
+            if(act.equals("D")){
+                if(direction == 'N'){
+                    direction = 'E';
+                }
+                if(direction == 'O'){
+                    direction = 'N';
+                }
+                if(direction == 'S'){
+                    direction = 'O';
+                }
+                if(direction == 'E'){
+                    direction = 'S';
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+/*
+        initialisation();
+        while(true){
+            ArrayDeque<String> file = creationFile();
+            deplacement(file);
+            System.out.println(position);
+            System.out.println(direction);
+        }
+*/
+    gen_map();
+    breadthFirstSearch(map, 13);
+    depthFirstSearch(map, 13);
+    }
+
+    static void gen_map(){
+        map.put(13, new Integer[]{8, 17});
+        map.put(8, new Integer[]{1, 11});
+        map.put(1, new Integer[]{6});
+        map.put(6, new Integer[]{});
+        map.put(11, new Integer[]{});
+        map.put(17, new Integer[]{15, 25});
+        map.put(15, new Integer[]{});
+        map.put(25, new Integer[]{22, 27});
+        map.put(22, new Integer[]{});
+        map.put(27, new Integer[]{});
+    }
+
+    static void breadthFirstSearch(TreeMap<Integer, Integer[]> graphe, Integer depart){
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        HashSet<Integer> discovered = new HashSet<>();
+        ArrayList<Integer> path = new ArrayList<>();
+        queue.addFirst(depart);
+        while(queue.size()>0){
+            Integer node = queue.removeLast();
+            if(!discovered.contains(node)){
+                discovered.add(node);
+                path.add(node);
+                for(Integer voisin : map.get(node)){
+                    queue.addFirst(voisin);
+                }
+            }
+        }
+        System.out.println(path);
+    }
+
+    static void depthFirstSearch(TreeMap<Integer, Integer[]> graphe, Integer depart){
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        HashSet<Integer> discovered = new HashSet<>();
+        ArrayList<Integer> path = new ArrayList<>();
+        stack.addFirst(depart);
+        while(stack.size()>0){
+            Integer node = stack.removeFirst();
+            if(!discovered.contains(node)){
+                discovered.add(node);
+                path.add(node);
+                for(Integer voisin : map.get(node)){
+                    stack.addFirst(voisin);
+                }
+            }
+        }
+        System.out.println(path);
+    }
+}
+
